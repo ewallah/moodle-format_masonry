ba7d84e2110f46182d3dd0a5fe97225cbe41b7d0
ewallah <info@ewallah.net>
2013-12-09 20:48:17 +0100

    Update version.php

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013031600;         // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2012110900;         // Requires this Moodle version (2.4).
+$plugin->version   = 2013120100;         // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2013110500;         // Requires this Moodle version (2.6).
 $plugin->component = 'format_masonry';   // Full name of the plugin (used for diagnostics).
-$plugin->release  = '1.1';
+$plugin->release  = '1.2';
 $plugin->maturity = MATURITY_RC;
 $plugin->dependencies = array(
-    'format_topics' => 2012112900,
+    'format_topics' => 2013110500,
 );
