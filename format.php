bfc80b829e181dd47965ca7f2549342a80049a08
ewallah <info@ewallah.net>
2013-12-09 19:39:20 +0100

    Update format.php

--- format.php
+++ format.php
@@ -24,15 +24,22 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-// Make sure all sections are created.
+require_once($CFG->libdir.'/filelib.php');
+require_once($CFG->libdir.'/completionlib.php');
+
 $course = course_get_format($course)->get_course();
-course_create_sections_if_missing($course, range(0, $course->numsections));
+$context = context_course::instance($course->id);
 
 // Handle currentsection.
 if (($marker >=0) && has_capability('moodle/course:setcurrentsection', $context) && confirm_sesskey()) {
     $course->marker = $marker;
     course_set_marker($course->id, $marker);
 }
+
+// Make sure all sections are created.
+course_create_sections_if_missing($course, range(0, $course->numsections));
+
+
 
 if ($PAGE->user_is_editing()) {
     // Rely on the standard topics rendering.
@@ -52,7 +59,7 @@ if ($PAGE->user_is_editing()) {
             array(
                'name' => 'course_format_masonry',
                'fullpath' => '/course/format/masonry/format.js',
-               'requires' => array('base', 'node', 'transition', 'event')
+               'requires' => array('base', 'node', 'transition', 'event', 'event-custom')
             )
     );
     $renderer = $PAGE->get_renderer('format_masonry');
