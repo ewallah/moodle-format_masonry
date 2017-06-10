39873c2fdcaa4becad80d602c17606460cdc6943
eWallah <info@ewallah.net>
2017-06-10 14:18:05 +0200

    version update

--- version.php
+++ version.php
@@ -18,17 +18,17 @@
  * Version details
  *
  * @package    format_masonry
- * @copyright  2016 Renaat Debleu (www.eWallah.net)
+ * @copyright  2017 Renaat Debleu (www.eWallah.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016111802;
-$plugin->requires  = 2016052300;
+$plugin->version   = 2017061000;
+$plugin->requires  = 2017051500;
 $plugin->component = 'format_masonry';
-$plugin->release  = '3.2';
+$plugin->release  = '3.3';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'format_topics' => 2016052300,
+    'format_topics' => 2017051500,
 );
