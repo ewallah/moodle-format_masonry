4f72877fe2c186998c452b1e0f1ac44e96c1ae93
eWallah <info@ewallah.net>
2017-06-10 13:02:21 +0200

    phpunit

---
+++ tests/format_masonry_test.php
@@ -0,0 +1,201 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * format_masonry related unit tests
+ *
+ * @package    format_masonry
+ * @copyright  2017 Renaat Debleu (www.eWallah.net)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once($CFG->dirroot . '/course/lib.php');
+
+/**
+ * format_masonry related unit tests
+ *
+ * @package    format_masonry
+ * @copyright  2017 Renaat Debleu (www.eWallah.net)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class format_masonry_testcase extends advanced_testcase {
+
+    /**
+     * Tests for format_masonry::get_section_name method with default section names.
+     */
+    public function test_get_section_name() {
+        global $DB;
+        $this->resetAfterTest(true);
+        $generator = $this->getDataGenerator();
+        $course = $generator->create_course(['numsections' => 5, 'format' => 'masonry'], ['createsections' => true]);
+        $coursesections = $DB->get_records('course_sections', ['course' => $course->id]);
+        $courseformat = course_get_format($course);
+        foreach ($coursesections as $section) {
+            // Assert that with unmodified section names, get_section_name returns the same result as get_default_section_name.
+            $this->assertEquals($courseformat->get_default_section_name($section), $courseformat->get_section_name($section));
+        }
+    }
+
+    /**
+     * Tests for format_masonry::get_section_name method with modified section names.
+     */
+    public function test_get_section_name_customised() {
+        global $DB;
+        $this->resetAfterTest(true);
+        $generator = $this->getDataGenerator();
+        $course = $generator->create_course(['numsections' => 5, 'format' => 'masonry'], ['createsections' => true]);
+        $coursesections = $DB->get_records('course_sections', ['course' => $course->id]);
+        // Modify section names.
+        $customname = "Custom Section";
+        foreach ($coursesections as $section) {
+            $section->name = "$customname $section->section";
+            $DB->update_record('course_sections', $section);
+        }
+
+        // Requery updated section names then test get_section_name.
+        $coursesections = $DB->get_records('course_sections', ['course' => $course->id]);
+        $courseformat = course_get_format($course);
+        foreach ($coursesections as $section) {
+            // Assert that with modified section names, get_section_name returns the modified section name.
+            $this->assertEquals($section->name, $courseformat->get_section_name($section));
+        }
+    }
+
+    /**
+     * Tests for format_masonry::get_default_section_name.
+     */
+    public function test_get_default_section_name() {
+        global $DB;
+        $this->resetAfterTest(true);
+        $generator = $this->getDataGenerator();
+        $course = $generator->create_course(['numsections' => 5, 'format' => 'masonry'], ['createsections' => true]);
+        $coursesections = $DB->get_records('course_sections', ['course' => $course->id]);
+        $courseformat = course_get_format($course);
+        foreach ($coursesections as $section) {
+            if ($section->section == 0) {
+                $sectionname = get_string('section0name', 'format_masonry');
+                $this->assertEquals($sectionname, $courseformat->get_default_section_name($section));
+            } else {
+                $sectionname = get_string('sectionname', 'format_masonry') . ' ' . $section->section;
+                $this->assertEquals($sectionname, $courseformat->get_default_section_name($section));
+            }
+        }
+    }
+
+    /**
+     * Test web service updating section name
+     */
+    public function test_update_inplace_editable() {
+        global $CFG, $DB, $PAGE;
+        require_once($CFG->dirroot . '/lib/external/externallib.php');
+
+        $this->resetAfterTest();
+        $generator = $this->getDataGenerator();
+        $user = $generator->create_user();
+        $this->setUser($user);
+        $course = $generator->create_course(['numsections' => 5, 'format' => 'masonry'], ['createsections' => true]);
+        $coursesections = $DB->get_records('course_sections', ['course' => $course->id]);
+        $courseformat = course_get_format($course);
+        $section = $DB->get_record('course_sections', ['course' => $course->id, 'section' => 2]);
+
+        try {
+            core_external::update_inplace_editable('format_masonry', 'sectionname', $section->id, 'New section name');
+            $this->fail('Exception expected');
+        } catch (moodle_exception $e) {
+            $this->assertEquals('Course or activity not accessible. (Not enrolled)',
+                    $e->getMessage());
+        }
+
+        $teacherrole = $DB->get_record('role', ['shortname' => 'editingteacher']);
+        $generator->enrol_user($user->id, $course->id, $teacherrole->id);
+
+        $res = core_external::update_inplace_editable('format_masonry', 'sectionname', $section->id, 'New section name');
+        $res = external_api::clean_returnvalue(core_external::update_inplace_editable_returns(), $res);
+        $this->assertEquals('New section name', $res['value']);
+        $this->assertEquals('New section name', $DB->get_field('course_sections', 'name', ['id' => $section->id]));
+    }
+
+    /**
+     * Test callback updating section name
+     */
+    public function test_inplace_editable() {
+        global $DB, $PAGE;
+
+        $this->resetAfterTest();
+        $generator = $this->getDataGenerator();
+        $user = $generator->create_user();
+        $course = $generator->create_course(['numsections' => 5, 'format' => 'masonry'], ['createsections' => true]);
+        $teacherrole = $DB->get_record('role', ['shortname' => 'editingteacher']);
+        $generator->enrol_user($user->id, $course->id, $teacherrole->id);
+        $this->setUser($user);
+        $section = $DB->get_record('course_sections', ['course' => $course->id, 'section' => 2]);
+
+        // Call callback format_masonry_inplace_editable() directly.
+        $tmpl = component_callback('format_masonry', 'inplace_editable', ['sectionname', $section->id, 'Rename me again']);
+        $this->assertInstanceOf('core\output\inplace_editable', $tmpl);
+        $res = $tmpl->export_for_template($PAGE->get_renderer('core'));
+        $this->assertEquals('Rename me again', $res['value']);
+        $this->assertEquals('Rename me again', $DB->get_field('course_sections', 'name', ['id' => $section->id]));
+
+        // Try updating using callback from mismatching course format.
+        try {
+            $tmpl = component_callback('format_weeks', 'inplace_editable', ['sectionname', $section->id, 'New name']);
+            $this->fail('Exception expected');
+        } catch (moodle_exception $e) {
+            $this->assertEquals(1, preg_match('/^Can not find data record in database/', $e->getMessage()));
+        }
+    }
+
+    /**
+     * Test get_default_course_enddate.
+     *
+     * @return void
+     */
+    public function test_default_course_enddate() {
+        global $CFG, $DB;
+
+        $this->resetAfterTest(true);
+        require_once($CFG->dirroot . '/course/tests/fixtures/testable_course_edit_form.php');
+        $this->setTimezone('UTC');
+        $generator = $this->getDataGenerator();
+        $params = ['format' => 'masonry', 'numsections' => 5, 'startdate' => 1445644800];
+        $course = $generator->create_course($params);
+        $category = $DB->get_record('course_categories', ['id' => $course->category]);
+
+        $args = [
+            'course' => $course,
+            'category' => $category,
+            'editoroptions' => [
+                'context' => context_course::instance($course->id),
+                'subdirs' => 0
+            ],
+            'returnto' => new moodle_url('/'),
+            'returnurl' => new moodle_url('/'),
+        ];
+
+        $courseform = new testable_course_edit_form(null, $args);
+        $courseform->definition_after_data();
+
+        $enddate = $params['startdate'] + get_config('moodlecourse', 'courseduration');
+
+        $weeksformat = course_get_format($course->id);
+        $this->assertEquals($enddate, $weeksformat->get_default_course_enddate($courseform->get_quick_form()));
+
+    }
+}
