2aff027baf9b426eb99dd76953d0d69687a6f8fa
eWallah <info@eWallah.net>
2020-07-27 10:28:35 +0200

    amd sectionlistener

--- format.php
+++ format.php
@@ -43,8 +43,8 @@ course_create_sections_if_missing($course, range(0, $course->numsections));
 
 if ($PAGE->user_is_editing()) {
     // Rely on the standard topics rendering.
-    $PAGE->requires->js_call_amd('core_course/sectionlistener');
     $PAGE->requires->js('/course/format/topics/format.js');
+    $PAGE->requires->js_call_amd('core_course/sectionlistener');
     $renderer = $PAGE->get_renderer('format_topics');
 } else {
     // Render using the masonry js.
