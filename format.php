add49a8ba994f2e332b756221355afd8ed6e99ce
eWallah <info@eWallah.net>
2020-07-27 11:31:26 +0200

    amd sectionlistener

--- format.php
+++ format.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once($CFG->libdir.'/filelib.php');
-require_once($CFG->libdir.'/completionlib.php');
+require_once($CFG->libdir . '/filelib.php');
+require_once($CFG->libdir . '/completionlib.php');
 
 $course = course_get_format($course)->get_course();
 $context = context_course::instance($course->id);
@@ -44,7 +44,9 @@ course_create_sections_if_missing($course, range(0, $course->numsections));
 if ($PAGE->user_is_editing()) {
     // Rely on the standard topics rendering.
     $PAGE->requires->js('/course/format/topics/format.js');
-    $PAGE->requires->js_call_amd('core_course/sectionlistener');
+    if ($CFG->version < 2020061500) {
+        $PAGE->requires->js_call_amd('core_course/sectionlistener');
+    }
     $renderer = $PAGE->get_renderer('format_topics');
 } else {
     // Render using the masonry js.
